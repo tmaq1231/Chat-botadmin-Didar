6bdd56d01a819c6f4f8542247b8fe36103a9a7ba
fahad1234-1234 <gamingfahad092@gmail.com>
2025-05-13 03:48:25 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🙂💦 | Goat Bot V2 ]";
+const doNotDelete = "[ 🦆 | Fahad Goat Bot V2 ]";
 /**
 * @author NTKhang
 * @author: do not delete it
@@ -406,4 +406,4 @@ function cropContent(content, max) {
 		content = content + "...";
 	}
 	return content;
-}
+						}
